1de4d607398455056030f2cc8975e5bb7e547961
Candela Landi <landicandela01@gmail.com>
2024-12-17 18:06:04 -0300

    finished

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -4,6 +4,7 @@ import { ImageListItem, ImageList } from '@mui/material';
 export const ImageGallery = () => {
 
   return (
+
     <ImageList sx={{ width: '100%', height: 500 }} cols={4} rowHeight={200}>
       { itemData.map((item) => (
         <ImageListItem key={item.img}>
@@ -16,6 +17,7 @@ export const ImageGallery = () => {
         </ImageListItem>
       ))}
     </ImageList>
+    
   );
 }
 
